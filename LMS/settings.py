dfe2b2e081bd429548fc73fb6ae894648002a658
Anish <you@example.com>
2021-05-09 08:56:26 +0545

    project completed recheck left

--- LMS/settings.py
+++ LMS/settings.py
@@ -62,7 +62,7 @@ ROOT_URLCONF = 'LMS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
